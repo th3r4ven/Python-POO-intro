ffa2c733b5a910943df7547ccfb6f0aba2f4b2b6
Matheus Chiarato <matheus.chiarato@outlook.com>
2021-01-15 16:25:39 -0300

    Chapter 3, implementing methods

--- account.py
+++ account.py
@@ -1,20 +1,42 @@
+from dates import Date
 
 
 class Account:
 
     def __init__(self, number, owner, balance, limit):
-        print('Creating object... {}'.format(self))
         self.number = number  # Class Attribute
         self.owner = owner
         self.balance = balance
         self.limit = limit
 
-    def create(self):
-        pass
+    def deposit(self, value):
+        self.balance += value
+        print("Successful, you deposit " + str(value) + " in your account")
+
+    def withdraw(self, value):
+        if value <= self.balance:
+            self.balance -= value
+            print("You withdraw $" + str(value))
+        else:
+            print("Not enough balance")
+
+    def statement(self):
+        print("Your bank statement is " + str(self.balance))
 
 
 if __name__ == '__main__':
-    account = Account(666, "Raven", 10000000, 999999)
+    account = Account(666, "Raven", 15, 999999)
 
     # Accessing class attribute
-    print(account.balance)
+    print("Accessing attribute directly: " + str(account.balance))
+
+    # Accessing class method (class functions)
+    account.statement()
+
+    account.withdraw(10)
+    account.deposit(100)
+    account.statement()
+
+    # Challenge
+    d = Date(15, '01', 2021)
+    d.formated()
