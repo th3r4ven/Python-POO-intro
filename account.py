5fae1a3be46c52061b21582eeae1b359c742a7c0
Matheus Chiarato <matheus.chiarato@outlook.com>
2021-01-15 15:07:30 -0300

    Chapter 1, creating my first class

---
+++ account.py
@@ -0,0 +1,17 @@
+
+
+class Account:
+
+    def __init__(self, number, owner, balance, limit):
+        print('Creating object... {}'.format(self))
+        self.number = number  # Class Attribute
+        self.owner = owner
+        self.balance = balance
+        self.limit = limit
+
+    def create(self):
+        pass
+
+
+if __name__ == '__main__':
+    Account(666, "Raven", 10000000, 999999)
